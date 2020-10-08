9dff4ef585b111de72bc2a30ba1bbc738b102d26
Johannes Otterbach <johannesotterbach@gmail.com>
2020-10-08 21:28:28 +0200

    add test for spectral density and stochastic lanczos quadrature

--- scitorch/tests/test_lanczos.py
+++ scitorch/tests/test_lanczos.py
@@ -5,7 +5,7 @@ from typing import Callable
 import numpy as np
 import torch
 
-from scitorch.lanczos import lanczos
+from scitorch.lanczos import lanczos, stochastic_lanczos_quadrature, spectral_density_estimation
 
 
 def test_lanczos():
@@ -29,3 +29,25 @@ def test_lanczos():
                                          np.sort(EV[:, 0].data.numpy()),
                                          decimal=4)
 
+
+def test_spectral_density_estimator():
+    """
+    Uses Wigner's semi-circle distribution to test this
+    """
+
+    torch.manual_seed(42)
+    D = 1000
+    A = torch.randn(D, D)
+    A = A + A.T
+    Avp = lambda v: A @ v
+    nodes, weights = stochastic_lanczos_quadrature(Avp, D, 100, 40, True)
+
+    bins = np.linspace(-100, 100, 101)
+    w = bins[1]-bins[0]
+    cnt, edges = np.histogram(np.linalg.eigvalsh(A.data.numpy()), bins=bins)
+
+    x = np.arange(-100, 100, .1)
+    spec = spectral_density_estimation(x, nodes, weights, 1.)
+
+
+    assert ((spec[np.digitize(edges, x)-1][:-1] - cnt/(w * cnt.sum()))**2).sum() < 1e-4
